2ad5a11a50c9acc4cb8d8f3013ee0424d08f3a79
DuniExchange <duniexchange@gmail.com>
2021-11-15 20:19:10 +0700

    first commit

---
+++ Demogit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demogit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELOOOOOOOOOOOOOOOOO SOMETHING");
+        }
+    }
+}
